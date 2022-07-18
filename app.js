d6bef301af246002643978c37a60db63920db7f9
Hafiz Muhammad Rayyan <99716542+HafizMuhammadRayyan@users.noreply.github.com>
2022-07-18 22:14:45 +0500

    2nd commit

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
+// let page = location.href.split("/");
+// page = page[page.length - 1];
 
-function signup() {
+let users = []
+
+function signup() {     // Sign Up Funtions
+
     let fullName = document.getElementById("fullName").value;
+    let city = document.getElementById("city").value
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
     let confirmPassword = document.getElementById("confirmPassword").value;
-    if (password != confirmPassword) {
+    // console.log(fullName);
+
+    if (password === confirmPassword) {
+        
+        let newUser = {
+            user_fullName: fullName,
+            user_city: city, 
+            user_email: email,
+            user_password: password,
+            user_confirmPassword: confirmPassword
+        }
+        console.log(newUser);
+
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].user_email === email) {
+                alert("Email already exists")
+                return;
+            }
+        }
+
+        users.push(newUser)
+
+        localStorage.setItem("users", JSON.stringify(users))
+        window.location.href = "./login.html";
+
+    }
+    else{
         alert("Passwords do not match");
         return;
     }
+    
 }
